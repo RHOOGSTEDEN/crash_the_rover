3daa28c75d00a401f73e008eaaa2d3d3fc5eba22
Class Account <ee106a-aan@imail.EECS.Berkeley.EDU>
2018-11-12 16:54:41 -0800

    Project Update 11/12/18 5pm

---
+++ project/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa18/class/ee106a-aan/ros_workspaces/project/src
+# Build directory: /home/cc/ee106a/fa18/class/ee106a-aan/ros_workspaces/project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(ar_track_alvar)
+SUBDIRS(proj)
---
+++ project/build/proj/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa18/class/ee106a-aan/ros_workspaces/project/src/proj
+# Build directory: /home/cc/ee106a/fa18/class/ee106a-aan/ros_workspaces/project/build/proj
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

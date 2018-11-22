b22b1434bed59708f835e79328e60c060918d4e3
Mariyam Jivani <mariyamjivani@berkeley.edu>
2018-11-22 11:43:55 -0800

    Update from Thanksgiving with change to the way in whihc rospytime was used

--- project/src/proj/src/turtlebot_control.py
+++ project/src/proj/src/turtlebot_control.py
@@ -11,7 +11,41 @@ import tf2_ros
 import sys
 from geometry_msgs.msg import Twist
 
-#Define the method which contains the main functionality of the node.
+
+def goToAR(turtlebot_frame, goal_frame, trans):
+    """
+    Performs the calculations needed to have the turtlebot approach the AR Tag if it is seen
+    If there is no AR Tag, the turtlebot stops. 
+    Inputs:
+    - turtlebot_frame: the tf frame of the AR tag on your turtlebot
+    - target_frame: the tf frame of the target AR tag
+    - trans: instance of the lookup transform 
+    """
+
+    # P-Constants 
+    K1 = 0.3
+    K2 = 1
+    # D-Constants 
+    control_command = Twist()
+    x = trans.transform.translation.x
+    y = trans.transform.translation.y
+    z = trans.transform.translation.z
+    # 0.55 is when the 17.0 cm AR Tag is flush with the floor
+    # NEED TO CHANGE once height determined
+    if (x > 0.55):
+        #print(x)
+        next_x = K1*x
+        next_theta = K2*y
+        control_command.linear.x = next_x  
+        control_command.linear.y = 0
+        control_command.linear.z = 0
+        control_command.angular.x = 0
+        control_command.angular.y = 0
+        control_command.angular.z = next_theta
+    return control_command
+
+
+#Method which contains the main functionality of the node.
 def controller(turtlebot_frame, goal_frame):
   """
   Controls a turtlebot whose position is denoted by turtlebot_frame,
@@ -21,75 +55,42 @@ def controller(turtlebot_frame, goal_frame):
   - turtlebot_frame: the tf frame of the AR tag on your turtlebot
   - target_frame: the tf frame of the target AR tag
   """
-
-  ################################### YOUR CODE HERE ##############
-
   #Create a publisher and a tf buffer, which is primed with a tf listener
   pub = rospy.Publisher("/mobile_base/commands/velocity" , Twist, queue_size=10)
   tfBuffer = tf2_ros.Buffer()
   tfListener = tf2_ros.TransformListener(tfBuffer)
   
+
   # Create a timer object that will sleep long enough to result in
   # a 10Hz publishing rate
-  r = rospy.Rate(10) # 10hz
-
-  K1 = 0.3
-  K2 = 1
+  r = rospy.Rate(20) # 10hz
   # Loop until the node is killed with Ctrl-C
   while not rospy.is_shutdown():
-    try:
 
-      canTrans = tfBuffer.can_transform(turtlebot_frame, goal_frame, rospy.Time())
-      print(canTrans)
-      if(canTrans == 0):
-        control_command = Twist()
-      else:
-      
-        trans = tfBuffer.lookup_transform(turtlebot_frame, goal_frame, rospy.Time())
+    canTrans = tfBuffer.can_transform(turtlebot_frame, goal_frame, rospy.Time.now(), rospy.Duration(0.5))
+    print(canTrans)
+    control_command = Twist()
 
-        x = trans.transform.translation.x
-        y = trans.transform.translation.y
-        z = trans.transform.translation.z
-        control_command = Twist()
-        # 0.55 is when the 17.0 cm AR Tag is flush with the floor
-        # NEED TO CHANGE once height determined
-        if (x > 0.55):
-          print(x)
-          next_x = K1*x
-          next_theta = K2*y
-          tw = Twist()
-          tw.linear.x = next_x  
-          tw.linear.y = 0
-          tw.linear.z = 0
-          tw.angular.x = 0
-          tw.angular.y = 0
-          tw.angular.z = next_theta
-          # Process trans to get your state error
-          # Generate a control command to send to the robot
-          control_command = tw
-
-      
-     
-      #################################### end your code ###############
-
-      pub.publish(control_command)
-    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-      print e
-      pass
+    if(canTrans == 1):
+        try:
+            trans = tfBuffer.lookup_transform(turtlebot_frame, goal_frame, rospy.Time.now(), rospy.Duration(0.5))
+            control_command = goToAR(turtlebot_frame, goal_frame, trans)
+              # Process trans to get your state error
+              # Generate a control command to send to the robot
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+          print e
+          pass
     # Use our rate object to sleep until it is time to publish again
+    pub.publish(control_command)
     r.sleep()
 
       
 # This is Python's sytax for a main() method, which is run by default
 # when exectued in the shell
 if __name__ == '__main__':
-  # Check if the node has received a signal to shut down
-  # If not, run the talker method
-
   #Run this program as a new node in the ROS computation graph 
   #called /turtlebot_controller.
   rospy.init_node('turtlebot_controller', anonymous=True)
-
   try:
     controller(sys.argv[1], sys.argv[2])
   except rospy.ROSInterruptException:

e43d6def75d46c8dcac4ec1e66c9571fb51ed7de
Mariyam Jivani <mariyamjivani@berkeley.edu>
2018-12-01 11:10:23 -0800

    Added a new control file, still need to test it

---
+++ project/src/proj/src/control_2.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+#The line above tells Linux that this file is a Python script,
+#and that the OS should use the Python interpreter in /usr/bin/env
+#to run it. Don't forget to use "chmod +x [filename]" to make
+#this script executable.
+
+#Import the rospy package. For an import to work, it must be specified
+#in both the package manifest AND the Python file in which it is used.
+import rospy
+import tf2_ros
+import sys
+from geometry_msgs.msg import Twist
+
+#Define the method which contains the main functionality of the node.
+def controller(bot_frame, ar_frame):
+  """
+  Controls a turtlebot whose position is denoted by turtlebot_frame,
+  to go to a position denoted by target_frame
+
+  Inputs:
+  - turtlebot_frame: the tf frame of the AR tag on your turtlebot
+  - target_frame: the tf frame of the target AR tag
+  """
+
+  ################################### YOUR CODE HERE ##############
+
+  #Create a publisher and a tf buffer, which is primed with a tf listener
+  pub = rospy.Publisher("/mobile_base/commands/velocity" , Twist, queue_size=10)
+  tfBuffer = tf2_ros.Buffer()
+  tfListener = tf2_ros.TransformListener(tfBuffer)
+  
+  # Create a timer object that will sleep long enough to result in
+  # a 10Hz publishing rate
+  r = rospy.Rate(10) # 10hz
+  
+  # Ccontroller Constants
+  K1 = 0.3
+  K2 = 1
+
+  world_frame = "odom"
+  isObstacle = False
+  # Loop until the node is killed with Ctrl-C
+  while not rospy.is_shutdown():
+    try:
+      
+      transWorldAR = tfBuffer.lookup_transform(world_frame, ar_frame, rospy.Time())
+      WAr_X = transWorldAR.transform.translation.x
+      WAr_Y = transWorldAR.transform.translation.y
+      WAr_Z = transWorldAR.transform.translation.z
+
+
+      transBotAR = tfBuffer.lookup_transform(frame, ar_frame, rospy.Time())
+      BAr_X = transBotAR.transform.translation.x
+      BAr_Y = transBotAR.transform.translation.y
+      BAr_Z = transBotAR.transform.translation.z
+
+      control_command = Twist()
+
+      # Check if there is an obstacle in your straight path to the AR Tag (Can use laser scan ideas from labs)
+
+
+
+
+      # If there is no obstacle, go to AR
+      if (isObstacle== False):
+        if (BAr_X > 0.55):
+        print("Heading to AR Tag:")
+        next_x = K1*BAr_X
+        next_theta = K2*BAr_Y
+
+        control_command.linear.x = next_x  
+        control_command.linear.y = 0
+        control_command.linear.z = 0
+        control_command.angular.x = 0
+        control_command.angular.y = 0
+        control_command.angular.z = next_theta
+        pub.publish(control_command)
+
+
+
+        
+    except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
+      print e
+      pass
+    # Use our rate object to sleep until it is time to publish again
+    r.sleep()
+
+      
+# This is Python's sytax for a main() method, which is run by default
+# when exectued in the shell
+if __name__ == '__main__':
+  # Check if the node has received a signal to shut down
+  # If not, run the talker method
+
+  #Run this program as a new node in the ROS computation graph 
+  #called /turtlebot_controller.
+  rospy.init_node('turtlebot_controller', anonymous=True)
+
+  try:
+    controller(sys.argv[1], sys.argv[2])
+  except rospy.ROSInterruptException:
+    pass
